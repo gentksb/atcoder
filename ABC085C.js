86b58b85e6895ef88eb64246c5e3b0a5fe40c1a6
gensobunya <osa.gensobunya@gmail.com>
2019-03-26 22:51:47 +0900

    otoshidama Clear!

--- ABC085C.js
+++ ABC085C.js
@@ -2,47 +2,34 @@
 
 function Main(input){
   const tmpInputs = input.toString().split(" ");
-  const N = tmpInputs[0];
-  const Y = tmpInputs[1];
+  const N = parseInt(tmpInputs[0],10);
+  const Y = parseInt(tmpInputs[1],10);
   const NoAnswer = "-1 -1 -1";
-//10000x+5000y+1000z=Y,x+y+z=N の連立方程式
-// {"10000":x,"5000":y,"1000":z}
-  let x = 0;
-  let y = 0;
-  let z = 0;
-//  const Otoshidama = {"10000円":x,"5000円":y,"1000円":z};
-  let answer = "";
 
-//組み合わせを一つ上げればいいので、最初の解を抽出したい
-//お札1枚づつ動かして条件ヒットしたらbreak
-//計算量がマッハ
-  for(let i=0;i<=N;i++){
-    x = N-i;
-    if (x+y+z==N && 10000*x+5000*y+1000*z == Y){
-      answer = x+" "+y+" "+z;
-      break
-    }
-    else{
-      for(let j=0;j<=N-x;j++){
-        y = N-j;
-        if (x+y+z==N && 10000*x+5000*y+1000*z == Y){
-          answer = x+" "+y+" "+z;
-          break
-        }
-        else{
-          for(let k=0;k<=N-(x+y);k++){
-            z = N-k;
-            if (x+y+z==N && 10000*x+5000*y+1000*z == Y){
-              answer = x+" "+y+" "+z;
-              break
-            }
-          }
-        }
+//可能な限り多く1万円札で作って、細かい部分が作れるか1枚づつ変更して確認していく
+
+  for(let YukichiCount=N; YukichiCount >= 0; YukichiCount--){
+    let extraYen_Y = Y - 10000*YukichiCount;
+    let extraBill_Y = N - YukichiCount
+    if(extraYen_Y<0){continue}
+
+      for(let IchiyoCount=extraBill_Y;IchiyoCount>=0;IchiyoCount--){
+        let extraYen_I = extraYen_Y - 5000*IchiyoCount
+        let extraBill_I = extraBill_Y - IchiyoCount
+        if(extraYen_I<0){continue}
+          
+          //1000円札は1パターンのみでOK
+          let HideyoCount = extraBill_I;
+          let extraYen = extraYen_I - 1000*HideyoCount;
+          if(extraYen!==0){continue}
+            console.log(YukichiCount+" "+IchiyoCount+" "+HideyoCount)
+            return 0
       }
-    }
   }
-  answer!=""?console.log(answer):console.log(NoAnswer);
+  console.log(NoAnswer)
+  return 0
 }
+
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
