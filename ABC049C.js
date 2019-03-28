cda2cbd87df4d5e4b15279f6b6241b4846c8c934
gentksb <gen@gensobunya.net>
2019-03-28 15:44:48 +0900

    WIP

--- ABC049C.js
+++ ABC049C.js
@@ -7,26 +7,26 @@ function Main(input){
   const str3 = "eraser"
   const str4 = "erase"
 
-//endWith使ってヒットしたらその単語を消す。ヒットしなかったらループ終わり、文字が残っていたらNG
+//endWith使って後方一致検索し、ヒットした単語を除去。その後全件一致チェックしてヒットしなかったらループ終わり、文字が残っていたらNG
+
+//終了チェック
   const checkEndingWithStrs = (chkStr) =>{
-    if(chkStr.endsWith(str1)||chkStr.endsWith(str2)||chkStr.endsWith(str3)||chkStr.endsWith(str4) ){
+    if(chkStr.endsWith(str1)||chkStr.endsWith(str2)||chkStr.endsWith(str3)||chkStr.endsWith(str4)){
       return 1;
     }else{
       return 0
     }
   }
 
-//dreamdreamer
-
   while(checkEndingWithStrs(baseStr)){
     baseStr.endsWith(str1) ? baseStr = baseStr.slice(0,baseStr.lastIndexOf(str1)):"";
     baseStr.endsWith(str2) ? baseStr = baseStr.slice(0,baseStr.lastIndexOf(str2)):"";
     baseStr.endsWith(str3) ? baseStr = baseStr.slice(0,baseStr.lastIndexOf(str3)):"";
     baseStr.endsWith(str4) ? baseStr = baseStr.slice(0,baseStr.lastIndexOf(str4)):"";
-    console.log(baseStr)
+    // console.log(baseStr+"="+checkEndingWithStrs(baseStr))
   }
 
-  baseStr.length == 0 ? console.log("YES") : console.log("NO");
+  baseStr.length === 0 ? console.log("YES") : console.log("NO");
 
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
